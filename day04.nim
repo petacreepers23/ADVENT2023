038e7ebf68e34ee195b4797877caf85521fda06d
petacreepers23 <javiermiguel4b@gmail.com>
2023-12-04 12:25:36 +0100

    day04 completed

---
+++ day04.nim
@@ -0,0 +1,66 @@
+import std/[sequtils, strutils, sugar, tables, enumerate]
+
+proc parse(filename:string):seq[seq[seq[string]]] =
+    var file = open(filename)
+    for line in file.lines:
+        let result_numbers = #seq[0]->result; seq[1]->numbers
+            line
+                .split(":")[1]
+                .split("|")
+                .map(x => x
+                        .strip
+                        .split(" ")
+                        .filter(x => x != ""))
+        result.add(result_numbers)
+    return result
+
+proc getPointsOfCard(result_numbers:seq[seq[string]]):int64 =
+    for possible_number in result_numbers[1]:
+        if(possible_number in result_numbers[0]):
+            if(result == 0):
+                result = 1
+            else:
+                result = result * 2
+    return result
+
+proc part01(filename:string):int64 =
+    for result_numbers in parse(filename):
+        result = result + getPointsOfCard(result_numbers)
+    return result
+
+proc getMatchingNumbers(result_numbers:seq[seq[string]]):int =
+    for possible_number in result_numbers[1]:
+        if(possible_number in result_numbers[0]):
+                result = result + 1
+    return result
+
+type CardValue = object
+    ammount_winners:int
+    instances:int
+    #winners:seq[string]
+    #numbers:seq[string]
+
+proc part02(filename:string):int64 =
+    var table = initOrderedTable[int,CardValue]()#card_number, values
+    for (i,result_numbers) in enumerate(parse(filename)):
+        let winning_numbers = getMatchingNumbers(result_numbers)
+        table[i+1] = CardValue(
+            ammount_winners:winning_numbers,
+            instances:1
+        )
+    for i in table.keys:   
+        for _ in countup(1,table[i].instances):
+            for extra in countup(i+1,i+table[i].ammount_winners):
+                table[extra].instances = table[extra].instances+1
+    #echo table
+    for card in table.values:
+        result = result + card.instances
+    #echo result
+
+proc main() =
+    doAssert(part01("inputs/example_1_day04.txt")==13)
+    doAssert(part02("inputs/example_1_day04.txt")==30)
+    echo "Parte 1: " & $part01("inputs/day04.txt")
+    echo "Parte 2: " & $part02("inputs/day04.txt")
+when isMainModule:
+  main()
